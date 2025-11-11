3557baaf4e5d26dd492160bb1ea5408bce233ca4
HyungJinHan <han1210_36@naver.com>
2025-11-11 13:34:51 +0900

    test: notion post migration test

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -4,8 +4,7 @@ const moment = require('moment');
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
-// or
-// import {NotionToMarkdown} from "notion-to-md";
+const yaml = require('js-yaml');
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -38,13 +37,15 @@ function replaceTitleOutsideRawBlocks(body) {
   return body;
 }
 
-// passing notion client to the option
 const n2m = new NotionToMarkdown({ notionClient: notion });
 
 (async () => {
-  // ensure directory exists
-  const root = '_posts';
+  const root = '_posts/notion';
   fs.mkdirSync(root, { recursive: true });
+
+  const templatePath = path.join('assets', 'template.md');
+  const templateContent = fs.readFileSync(templatePath, 'utf-8');
+  const templateFm = yaml.load(templateContent.split('---')[1].trim());
 
   const databaseId = process.env.DATABASE_ID;
   let response = await notion.databases.query({
@@ -75,18 +76,30 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
 
   for (const r of pages) {
     const id = r.id;
+    let frontmatter = { ...templateFm };
+
     // date
     let date = moment(r.created_time).format('YYYY-MM-DD');
     let pdate = r.properties?.['날짜']?.['date']?.['start'];
     if (pdate) {
       date = moment(pdate).format('YYYY-MM-DD');
     }
+    frontmatter.date = date;
+
     // title
     let title = id;
     let ptitle = r.properties?.['게시물']?.['title'];
     if (ptitle?.length > 0) {
       title = ptitle[0]?.['plain_text'];
     }
+    frontmatter.title = title;
+
+    // description
+    let pdesc = r.properties?.['설명']?.['rich_text'];
+    if (pdesc?.length > 0) {
+      frontmatter.description = pdesc[0]?.['plain_text'];
+    }
+
     // tags
     let tags = [];
     let ptags = r.properties?.['태그']?.['multi_select'];
@@ -96,6 +109,10 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
         tags.push(n);
       }
     }
+    if (tags.length > 0) {
+      frontmatter.tags = tags;
+    }
+
     // categories
     let cats = [];
     let pcats = r.properties?.['카테고리']?.['multi_select'];
@@ -105,30 +122,64 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
         cats.push(n);
       }
     }
-
-    // frontmatter
-    let fmtags = '';
-    let fmcats = '';
-    if (tags.length > 0) {
-      fmtags += '\ntags: [';
-      for (const t of tags) {
-        fmtags += t + ', ';
-      }
-      fmtags += ']';
-    }
     if (cats.length > 0) {
-      fmcats += '\ncategories: [';
-      for (const t of cats) {
-        fmcats += t + ', ';
-      }
-      fmcats += ']';
+      frontmatter.categories = cats;
     }
-    const fm = `---
-layout: post
-date: ${date}
-title: "${title}"${fmtags}${fmcats}
----
-`;
+
+    // pin
+    frontmatter.pin = r.properties?.['고정']?.['checkbox'] || false;
+    // math
+    frontmatter.math = r.properties?.['수학']?.['checkbox'] || false;
+    // mermaid
+    frontmatter.mermaid = r.properties?.['Mermaid']?.['checkbox'] || false;
+    // published
+    frontmatter.published = r.properties?.['공개']?.['checkbox'] || false;
+    // private
+    frontmatter.private = r.properties?.['숨김']?.['checkbox'] || false;
+
+    // image
+    let pimage = r.properties?.['대표 이미지']?.['files'];
+    if (pimage?.length > 0) {
+      const imageUrl =
+        pimage[0]?.['file']?.['url'] || pimage[0]?.['external']?.['url'];
+      if (imageUrl) {
+        const imageName = path.basename(new URL(imageUrl).pathname);
+        const imagePath = path.join(
+          frontmatter['image-path'],
+          title.replaceAll(' ', '-'),
+          imageName
+        );
+        frontmatter.image.path = imagePath;
+        frontmatter.image.alt = title;
+
+        const localImagePath = path.join(
+          'assets/img',
+          title.replaceAll(' ', '-'),
+          imageName
+        );
+        const dirname = path.dirname(localImagePath);
+        if (!fs.existsSync(dirname)) {
+          fs.mkdirSync(dirname, { recursive: true });
+        }
+
+        axios({
+          method: 'get',
+          url: imageUrl,
+          responseType: 'stream',
+        })
+          .then(function (response) {
+            response.data.pipe(fs.createWriteStream(localImagePath));
+          })
+          .catch(function (error) {
+            console.log('Image download error:', error);
+          });
+      }
+    } else {
+      delete frontmatter.image;
+    }
+
+    const fm = `---\n${yaml.dump(frontmatter)}---`;
+
     const mdblocks = await n2m.pageToMarkdown(id);
     let md = n2m.toMarkdownString(mdblocks)['parent'];
     if (md === '') {
@@ -143,7 +194,7 @@ title: "${title}"${fmtags}${fmcats}
     let edited_md = md.replace(
       /!\[(.*?)\]\((.*?)\)/g,
       function (match, p1, p2, p3) {
-        const dirname = path.join('assets/img', ftitle);
+        const dirname = path.join('assets/img/notion', ftitle);
         if (!fs.existsSync(dirname)) {
           fs.mkdirSync(dirname, { recursive: true });
         }
@@ -170,8 +221,7 @@ title: "${title}"${fmtags}${fmcats}
       }
     );
 
-    //writing to file
-    fs.writeFile(path.join(root, ftitle), fm + edited_md, (err) => {
+    fs.writeFile(path.join(root, ftitle), fm + '\n\n' + edited_md, (err) => {
       if (err) {
         console.log(err);
       }
