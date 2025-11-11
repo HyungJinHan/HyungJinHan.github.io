7c5d871c45492caeb3749ec20ac84fcc0164f97a
HyungJinHan <han1210_36@naver.com>
2025-11-11 11:52:09 +0900

    [배포] Notion 변경 사항 저장

--- _posts/computer-science/object-oriented_programming/oop-polymorphism/example-code/Driver.java
+++
@@ -1,7 +0,0 @@
-public class Driver {
-  // 매개 변수로 인터페이스 타입의 참조 변수를 전달
-  void drive(Vehicle vehicle) {
-    vehicle.moveForward();
-    vehicle.moveBackward();
-  }
-}
--- _posts/design-pattern/structural-pattern/composite/example-code/item_example/component.ts
+++
@@ -1,7 +0,0 @@
-/** Component 인터페이스 */
-interface IItemComponent {
-  getPrice(): number;
-  getName(): string;
-}
-
-export { IItemComponent };
